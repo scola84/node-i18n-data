d833733d9535f2c2087df2291969969e554c7e76
M.C. van den Boogaart <scola@kvdb.name>
2016-08-11 20:52:47 +0200

    Add data to i18n directly

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-import merge from 'lodash-es/merge';
-
 import { data as httpData } from '@scola/api-http';
 import { data as modelData } from '@scola/api-model';
 import { data as routerData } from '@scola/api-router';
 import { data as validatorData } from '@scola/validator';
 import { data as wsData } from '@scola/api-ws';
 
-export const data = merge({},
-  httpData,
-  modelData,
-  routerData,
-  validatorData,
-  wsData
-);
+export function data(i18n) {
+  i18n.string()
+    .data(httpData)
+    .data(modelData)
+    .data(routerData)
+    .data(validatorData)
+    .data(wsData);
+}
